563ce8d55d6c3e8f233fcfe405b323de00bd9cd5
Jyotibissoyi <jyotibissoyikhusbu2571@gmail.com>
2025-11-27 20:32:55 +0530

    bug fixed.

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const mongoose = require("mongoose");
 const route = require("./src/route");
 
 let app = express();
-
+app.use(express.json());
 app.use("/", route)
-mongoose.connect("mongodb+srv://Jyoticoder:Jyoti2025@jyoticoder-cluster.ljxxb2x.mongodb.net/Todo",()=>{
-    console.log("MongoDb connected")
-})
+mongoose.connect("mongodb+srv://Jyoticoder:Jyoti2025@jyoticoder-cluster.ljxxb2x.mongodb.net/Todo")
 
 
-app.connect(3000 || PORT ,()=>{
+app.listen(3000 || PORT ,()=>{
     console.log("Server is running")
 });
